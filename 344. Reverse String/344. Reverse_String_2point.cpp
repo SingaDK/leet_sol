6b311b0bff44eadfb3de43fcfa5b7f8b4544edc1
Singaravelan Neelakandan <30794545+SingaDK@users.noreply.github.com>
2020-05-06 23:45:22 +0200

    Create 344. Reverse_String_2point.cpp

---
+++ 344. Reverse String/344. Reverse_String_2point.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            swap(s.at(i), s.at(j));
+            i++;
+            j--;
+        }
+    }
+};
